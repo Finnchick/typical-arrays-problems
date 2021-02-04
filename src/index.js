56b34606492b0591c1047ced51b73647f7aaecd7
Finnchick <sergik3001@gmail.com>
2021-02-05 02:31:35 +0300

    implent funtcions for arrays: min, max, average

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  else return array.reduce((min, current) => min > current ? current : min);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  else return array.reduce((max, current) => max < current ? current : max);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  else return array.reduce((sum, current) => sum + current, 0) / array.length;
 }
